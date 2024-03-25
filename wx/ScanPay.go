7459681560840bcbd2af33a1d6254b49a25cc570
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-03-25 20:31:15 +0800

    add pay channel

--- wx/ScanPay.go
+++ wx/ScanPay.go
@@ -3,14 +3,12 @@ package wx
 import (
 	"bytes"
 	"crypto/md5"
-	"crypto/rand"
 	"encoding/hex"
 	"encoding/xml"
 	"fmt"
+	"github.com/open4go/r2id"
 	"io/ioutil"
-	"math/big"
 	"net/http"
-	"sort"
 	"strings"
 	"time"
 )
@@ -20,29 +18,9 @@ import (
 // https://pay.weixin.qq.com/wiki/doc/api/micropay.php?chapter=9_10&index=1
 // https://products.aspose.app/barcode/zh-hans/scanqr#/recognized
 
-// 生成随机字符串
-func generateRandomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var result strings.Builder
-	for i := 0; i < length; i++ {
-		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		result.WriteByte(charset[n.Int64()])
-	}
-	return result.String()
-}
-
-// 生成商户订单号
-func generateOutTradeNo() string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-|*"
-	var result strings.Builder
-	//timestamp := time.Now().UnixNano() / int64(time.Millisecond) // 获取当前时间戳（毫秒）
-	result.WriteString(time.Now().Format("20060102150405")) // 使用时间作为订单号的一部分
-	for i := 0; i < 32-len(result.String()); i++ {
-		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		result.WriteByte(charset[n.Int64()])
-	}
-	return result.String()
-}
+const (
+	MicroPayApi = "https://api.mch.weixin.qq.com/pay/micropay"
+)
 
 // RequestParams 请求参数结构体
 type RequestParams struct {
@@ -84,43 +62,46 @@ type SignStruct struct {
 	AuthCode       string `xml:"auth_code,omitempty"`
 }
 
-// BuildRequestParams 构建请求参数
-func BuildRequestParams() RequestParams {
-	return RequestParams{
-		// TODO 当前的appid是小程序的，有可能是导致签名错误的原因
-		AppID:          "**",
-		MchID:          "**",
-		NonceStr:       generateRandomString(32),
-		Body:           "image形象店-深圳腾大- QQ公仔",
-		OutTradeNo:     generateOutTradeNo(),
-		TotalFee:       888,
-		SpbillCreateIP: "8.8.8.8",
-		// AuthCode 扫码枪读取的支付二维码
-		AuthCode: "132882375849305673",
-	}
-}
-
 // GenerateSign 签名生成算法
 func GenerateSign(signStruct SignStruct, apiKey string) string {
 	var signSlice []string
 	signSlice = append(signSlice, fmt.Sprintf("appid=%s", signStruct.AppID))
 	//signSlice = append(signSlice, fmt.Sprintf("attach=%s", signStruct.Attach))
 	signSlice = append(signSlice, fmt.Sprintf("body=%s", signStruct.Body))
-	//signSlice = append(signSlice, fmt.Sprintf("device_info=%s", signStruct.DeviceInfo))
+	signSlice = append(signSlice, fmt.Sprintf("device_info=%s", signStruct.DeviceInfo))
 	signSlice = append(signSlice, fmt.Sprintf("mch_id=%s", signStruct.MchID))
 	signSlice = append(signSlice, fmt.Sprintf("nonce_str=%s", signStruct.NonceStr))
-	signSlice = append(signSlice, fmt.Sprintf("out_trade_no=%s", signStruct.OutTradeNo))
-	signSlice = append(signSlice, fmt.Sprintf("spbill_create_ip=%s", signStruct.SpbillCreateIP))
-	signSlice = append(signSlice, fmt.Sprintf("total_fee=%d", signStruct.TotalFee))
-	signSlice = append(signSlice, fmt.Sprintf("auth_code=%s", signStruct.AuthCode))
-	sort.Strings(signSlice)
+	//signSlice = append(signSlice, fmt.Sprintf("out_trade_no=%s", signStruct.OutTradeNo))
+	//signSlice = append(signSlice, fmt.Sprintf("spbill_create_ip=%s", signStruct.SpbillCreateIP))
+	//signSlice = append(signSlice, fmt.Sprintf("total_fee=%d", signStruct.TotalFee))
+	//signSlice = append(signSlice, fmt.Sprintf("auth_code=%s", signStruct.AuthCode))
+	//sort.Strings(signSlice)
 	signStr := strings.Join(signSlice, "&") + "&key=" + apiKey
+
+	fmt.Println("st-->", signStr)
 
 	// 默认签名类型为MD5
 	h := md5.New()
 	h.Write([]byte(signStr))
 	sign := hex.EncodeToString(h.Sum(nil))
 	return strings.ToUpper(sign)
+}
+
+// BuildRequestParams 构建请求参数
+func BuildRequestParams() RequestParams {
+	return RequestParams{
+		// TODO 当前的appid是小程序的，有可能是导致签名错误的原因
+		AppID:          "wx265399cdd2753331",
+		MchID:          "1525274081",
+		DeviceInfo:     "macbook",
+		NonceStr:       r2id.GenerateRandomString(32),
+		Body:           "image形象店-深圳腾大- QQ公仔",
+		OutTradeNo:     r2id.GenerateOutTradeNo(),
+		TotalFee:       888,
+		SpbillCreateIP: "8.8.8.8",
+		// AuthCode 扫码枪读取的支付二维码
+		AuthCode: "132882375849305673",
+	}
 }
 
 // SendRequest 发送请求
@@ -139,10 +120,9 @@ func SendRequest(params RequestParams) error {
 		SpbillCreateIP: params.SpbillCreateIP,
 		AuthCode:       params.AuthCode,
 	}
-	sign := GenerateSign(signStruct, "**")
+	sign := GenerateSign(signStruct, "Huanglijiguiliuluosifen199409277")
 
 	// 设置请求参数
-	fmt.Println("sign-->", sign)
 	params.Sign = sign
 	params.SignType = "MD5" // 默认签名类型为MD5
 
@@ -151,9 +131,7 @@ func SendRequest(params RequestParams) error {
 		return err
 	}
 
-	fmt.Println(string(xmlBody))
-
-	req, err := http.NewRequest("POST", "https://api.mch.weixin.qq.com/pay/micropay", bytes.NewBuffer(xmlBody))
+	req, err := http.NewRequest("POST", MicroPayApi, bytes.NewBuffer(xmlBody))
 	if err != nil {
 		return err
 	}
@@ -172,7 +150,7 @@ func SendRequest(params RequestParams) error {
 		return err
 	}
 
-	fmt.Println(string(body))
+	fmt.Println("---->", string(body), err)
 	return nil
 }
 
